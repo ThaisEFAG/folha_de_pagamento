817dae4ad63c7b50407ad71f8c9823fe20c4d335
Thaís <thaisefag1992@gmail.com>
2024-05-19 10:52:39 -0300

    addFeature

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// let -> sofre alteração durante a execução do programa
+// const -> valor fixo durante a execução do programa
+
+const text = "Thais elaine Farias Gonçalves"
+console.log(text.replace("elaine", 'EditCode'))
+
+
+const salario = "150";
+const adicionais = 500;
+
+console.log(Number(salario) + Number(adicionais))
