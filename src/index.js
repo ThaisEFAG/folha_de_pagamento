f8f17aef6c709761c934130c402d983d8be80bfe
Thaís <thaisefag1992@gmail.com>
2024-05-19 13:40:34 -0300

    nodemon-script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+const readline = require('readline');
+
 // let -> sofre alteração durante a execução do programa
 // const -> valor fixo durante a execução do programa
 
-const text = "Thais elaine Farias Gonçalves"
-console.log(text.replace("elaine", 'EditCode'))
+// 
 
+const input = readline.createInterface(
+    // Perguntas no teminal
+    // captura de processos de entrada de dados
+    process.stdin,
+    // captura de processos de saída de dados
+    process.stdout
+);
 
-const salario = "150";
-const adicionais = 500;
+let nome = "";
+let sobrenome = "";
 
-console.log(Number(salario) + Number(adicionais))
+input.question("qual o seu nome?", (nomeDigitado) => {
+ nome = nomeDigitado
+
+input.question("quak seu sobrenome?", (sobrenomeDigitado) => {
+        sobrenome = sobrenomeDigitado
+         input.close()
+         console.log(`${nome} ${sobrenome}`)
+})
+})
